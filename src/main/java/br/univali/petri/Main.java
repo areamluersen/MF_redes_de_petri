f676b47829f85aebf2d05c4550fc48a12c8966b5
Khalil Santana <khalilsantana@hotmail.com>
2021-07-30 16:59:50 -0300

    feat: receive models files from args

--- src/main/java/br/univali/petri/Main.java
+++ src/main/java/br/univali/petri/Main.java
@@ -6,7 +6,11 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws IOException {
         Path p = Path.of("models/pn-04.json");
-        System.out.println(p.toString());
+        if (args.length != 1) {
+            System.out.println("Model file not provided, using defaults");
+        } else {
+            p = Path.of(args[0]);
+        }
         var pn = PetriNetwork.fromModelFile(p);
         pn.run();
     }
